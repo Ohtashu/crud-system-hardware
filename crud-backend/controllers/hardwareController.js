f39ce35d02c53a9bfb5abac62321fa04a5b3a29e
Juan Carlos A. Flores <kunt0069@gmail.com>
2026-09-17 12:11:28 +0800

    feat: implement create functionality for hardware components

--- crud-backend/controllers/hardwareController.js
+++ crud-backend/controllers/hardwareController.js
@@ -14,6 +14,33 @@ const getAllComponents = async (req, res) => {
     }
 };
 
+//For CREATE (CRUD)
+const createComponents = async (req, res) => {
+    try {
+        const { hardware_name, category, brand, quantity, price } = req.body;
+
+        const pool = await poolPromise;
+
+        await pool.request()
+            .input('name', sql.VarChar, hardware_name)
+            .input('cat', sql.VarChar, category)
+            .input('brand', sql.VarChar, brand)
+            .input('qty', sql.Int, quantity)
+            .input('price', sql.Int, price)
+            .query(`
+                    INSERT INTO hardware(hardware_name, category, brand, quantity, price)
+                    VALUES (@name, @cat, @brand, @qty, @price)
+                  `);
+        res.status(201).json({ message: 'Hardware component added successfully!' });
+
+    } catch (error) {
+        console.error('Error creating component: ',error);
+        res.status(500).json({error: 'Failed to create hardware component'});
+
+    }
+};
+
 module.exports = {
-    getAllComponents
+    getAllComponents,
+    createComponents
 };
--- crud-backend/routes/components.js
+++ crud-backend/routes/components.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const hardwareController = require('../controllers/hardwareController');
 
 router.get('/', hardwareController.getAllComponents);
-
+router.post('/', hardwareController.createComponents);
 module.exports = router;
